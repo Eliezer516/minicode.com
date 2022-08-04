790afdd952313e61c8b5cb0808c3cce6f2cb04e0
Eliezer516 <ybarretomaterano@gmail.com>
2022-08-04 11:14:36 -0400

    fix localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -4,33 +4,7 @@ import './fontsize.js'
 import './theme.js'
 import './wordwrap.js'
 import './lblbutons.js'
-
-if (window.localStorage.getItem('editorValue')) {
-  const { html, css, js } = JSON.parse(window.localStorage.getItem('editorValue'))  
-  htmleditor.setOption('value', html)
-  csseditor.setOption('value', css)
-  jseditor.setOption('value', js)
-  const storageHtml = `
-    <!DOCTYPE html>
-    <html lang="es">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-      <style>
-        ${css}
-      </style>
-    </head>
-    <body>
-      ${html}
-      <script>
-        ${js}
-      </script>
-    </body>
-    </html>
-  `
-  updatePreview(storageHtml)
-}
+import './localstorage.js'
 
 function createHTML () {
   const html = htmleditor.getValue()
@@ -58,32 +32,15 @@ function createHTML () {
   `
 }
 
-function updatePreview (storageHtml) {
+export function updatePreview (storageHtml) {
   if (storageHtml) {
     $('iframe').setAttribute('srcdoc', storageHtml)
   }
 
   const html = createHTML()
-  $('iframe').setAttribute('srcdoc', '')
   $('iframe').setAttribute('srcdoc', html)
-}
-
-function setEitorValueInLocalStorage () {
-  const html = htmleditor.getValue()
-  const css = csseditor.getValue()
-  const js = jseditor.getValue()
-  const editorValue = {
-    html,
-    css,
-    js
-  }
-  window.localStorage.setItem('editorValue', JSON.stringify(editorValue))
 }
 
 $('#previewBtn').onclick = () => {
   updatePreview()
 }
-
-htmleditor.on('change', () => setEitorValueInLocalStorage())
-csseditor.on('change', () => setEitorValueInLocalStorage())
-jseditor.on('change', () => setEitorValueInLocalStorage())
